8633d71a9d9d81e26331a0a8b4177923d4b38db8
Marcin Matula <marcin.matula@gmail.com>
2023-04-30 12:27:17 +0200

    public JobHandling

--- lib.py
+++ lib.py
@@ -1,3 +1,4 @@
 from private.libjob import JobsProcess
 from private.libjob import parse_pipeline_file
 from private.libjob import _SignalHandler as SignalHandler
+from private.libjob import JobHandling
