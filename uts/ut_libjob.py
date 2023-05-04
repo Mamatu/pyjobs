a655c2a41f4e56d6a0c0ceabc721ab711eae7916
Marcin Matula <marcin.matula@gmail.com>
2023-05-04 19:32:02 +0200

    test_lib_pipelines_stop_handler impl

--- uts/ut_libjob.py
+++ uts/ut_libjob.py
@@ -100,10 +100,25 @@ def test_lib_pipelines_1():
 def test_lib_pipelines_stop_handler():
     from pyjobs import lib
     update_id = 0
-    pipelines = {(0, update_id) : [MagicMock(), MagicMock(), MagicMock()], (1, update_id) : [MagicMock(), MagicMock(), MagicMock()]}
+    mock_process = MagicMock()
+    mock_internal = MagicMock()
+    stopped = False
+    def side_effect():
+        time.sleep(0.5)
+        if not stopped:
+            mock_internal()
+    def stop_handler_side_effect(x):
+        stopped = x
+    mock_process.side_effect = side_effect
+    mock_stop_handler = MagicMock()
+    mock_stop_handler.side_effect = stop_handler_side_effect
+    another_pipeline = [MagicMock(), MagicMock(), MagicMock()]
+    pipelines = {(0, update_id) : [(mock_process, mock_stop_handler)], (1, update_id) : another_pipeline}
     jp = lib.JobsProcess()
     jp.process_pipelines(pipelines)
+    mock_stop_handler(True)
     jp.wait_for_finish(0)
-    for i in pipelines[(0, 0)]: i.assert_called_once()
     jp.wait_for_finish(1)
+    mock_stop_handler.assert_called_once()
+    mock_internal.assert_not_called()
     for i in pipelines[(1, 0)]: i.assert_called_once()
